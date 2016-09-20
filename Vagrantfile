640c41e5cb5a7ad65bb1700f32444acfea098052
Henry Jenkins <henry@henryjenkins.name>
2016-09-20 08:45:28 -0400

    use different base image
    New base image uses the default user of `ubuntu` rather than `vagrant`.

--- Vagrantfile
+++ Vagrantfile
@@ -2,8 +2,10 @@
 # vi: set ft=ruby :
 
 Vagrant.configure('2') do |config|
-  config.vm.box = "official-xenial-32"
-  config.vm.box_url = "https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-i386-vagrant.box"
+  config.vm.box = "ubuntu/xenial32"
+  config.vm.provider :virtualbox do |vb|
+      vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+  end
   config.vm.provision :shell, path: "provision-root.sh"
   config.vm.provision :shell, path: "provision-user.sh", privileged: false
   config.vm.provision :shell, path: "provision-gems.sh", privileged: false
--- provision-user.sh
+++ provision-user.sh
@@ -1,21 +1,21 @@
 # RailsBridge Montreal VM provision script (user part)
 
 # Copy files that should be owned by the user account
-rsync -rtv /vagrant/dotfiles/ /home/vagrant
+rsync -rtv /vagrant/dotfiles/ /home/ubuntu
 
 # Our bash setup will cd to workspace on login.
-cd /home/vagrant
+cd /home/ubuntu
 ln -s /vagrant workspace
 
 # Install rbenv and Ruby
-rm -rf /home/vagrant/.rbenv
-git clone https://github.com/rbenv/rbenv.git /home/vagrant/.rbenv
-git clone https://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
+rm -rf /home/ubuntu/.rbenv
+git clone https://github.com/rbenv/rbenv.git /home/ubuntu/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git /home/ubuntu/.rbenv/plugins/ruby-build
 
-source /home/vagrant/.bashrc
-/home/vagrant/.rbenv/bin/rbenv install 2.2.5
-/home/vagrant/.rbenv/bin/rbenv rehash
-/home/vagrant/.rbenv/bin/rbenv global 2.2.5
+source /home/ubuntu/.bashrc
+/home/ubuntu/.rbenv/bin/rbenv install 2.2.5
+/home/ubuntu/.rbenv/bin/rbenv rehash
+/home/ubuntu/.rbenv/bin/rbenv global 2.2.5
 
 # Heroku Toolbelt
 sudo wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
